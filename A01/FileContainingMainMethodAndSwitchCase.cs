fe3f2db24f4ef6b3d3d677027347e400071fbae0
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-08-07 15:15:10 +0530

    Added files for guessing game

---
+++ A01/FileContainingMainMethodAndSwitchCase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A01;
+
+class FileContainingMainMethodAndSwitchCase
+{
+    public static void Main(string[] args)
+    {   Console.WriteLine("Select a number that corresponds to the desired option:");
+        Console.WriteLine("1-Guessing Game : Academy1");
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice) {
+            case 1:
+                Academy1.academy1();
+                break;
+            default:
+                Console.WriteLine("Invalid choice");
+                break;
+        }
+    }
+     
+}
