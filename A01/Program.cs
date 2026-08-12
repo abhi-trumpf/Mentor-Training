84778c606e42f0df10b3b72a9f8016ceb2a484fa
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-08-12 18:13:41 +0530

    added necessary files for guess program

---
+++ A01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A01;
+
+    class Program {
+        public static void Run() {
+            int randomGeneratedNumber = new Random().Next(0, 101);  // generates a random number between 0 and 100 does not include 101
+                                                                    //Console.WriteLine(randomGeneratedNumber);
+            bool gameWon = false;
+            while (!gameWon) {
+                Console.WriteLine ("Enter your guess:");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine ($"Your guess is {guess}");
+                if (guess < randomGeneratedNumber){
+                    Console.WriteLine ("Your guess is too low");
+                }
+                else if (guess > randomGeneratedNumber){
+                    Console.WriteLine ("Your guess is too high");
+                }
+                else{
+                    Console.WriteLine ("You guessed it!");
+                    gameWon = true;
+                }
+            }
+        }
+
+    public static void Main(string[] args) {
+        Console.WriteLine ("Welcome to the Number Guessing Game!");
+                Program.Run ();
+        }
+}
