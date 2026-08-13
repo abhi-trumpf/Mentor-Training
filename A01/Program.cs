062b3c2e1539d4d42f73b1c548d1508519125a62
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-08-13 16:32:32 +0530

    added necessary spacing

--- A01/Program.cs
+++ A01/Program.cs
@@ -6,21 +6,21 @@ namespace A01;
 
 class Program{
     public static void Run () {
-        Console.WriteLine("Welcome to the number guessing game!");
-        Console.WriteLine("The range limit for the number to be guessed is between 1 and 100");
+        Console.WriteLine ("Welcome to the number guessing game!");
+        Console.WriteLine ("The range limit for the number to be guessed is between 1 and 100");
         int randomNumber = new Random().Next(1, 101);  //generates a random number between 1 and 100 does not include 101
         while (true){
             Console.WriteLine("Enter your guess:");
             if(!int.TryParse(Console.ReadLine(), out int guess)){
-                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                Console.WriteLine ("Invalid input. Please enter a valid integer.");
                 continue;
             }
             if(guess < randomNumber)
-                Console.WriteLine("Your guess is too low");
+                Console.WriteLine ("Your guess is too low");
             else if(guess > randomNumber)
-                Console.WriteLine("Your guess is too high");
+                Console.WriteLine ("Your guess is too high");
             else{
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine ("You guessed it!");
                 break;
             }
         }
