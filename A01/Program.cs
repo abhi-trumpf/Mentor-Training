eb90d9f55d1e0ff3c82f84c6523e20855001c2ca
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-08-17 13:14:22 +0530

    added header comment section and simplified the program

--- A01/Program.cs
+++ A01/Program.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- 2026.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------------------------------------
+// Program.cs
+// Program used to make the user guess a random number between 1 and 100. The user has to guess the number correctly, and the program will provide feedback on whether the guess is too low or too high. 
+// The program continues until the user guesses the correct number.
+// ------------------------------------------------------------------------------------------------
 namespace A01;
 
 class Program {
-   public static void Run () {
-      Console.WriteLine ("Welcome to the number guessing game!\nThe range limit for the number to be guessed is between 1 and 100") ;
+   public static void Main () {
+      Console.WriteLine ("Welcome to the number guessing game!\nThe range limit for the number to be guessed is between 1 and 100");
       int randomNumber = new Random ().Next (1, 101);  // generates a random number between 1 and 100 does not include 101
       while (true) {
          Console.Write ("Enter your guess: ");
-         if (!int.TryParse (Console.ReadLine (), out int guess) || guess < 0 || guess > 100) {
-            Console.WriteLine ("Invalid input. Please enter a valid integer.");
+         if (!int.TryParse (Console.ReadLine (), out int guess) || guess < 1 || guess > 100) {
+            Console.WriteLine ("Invalid input. Please enter a valid integer between 1 and 100.");
             continue;
          }
          if (guess < randomNumber)
@@ -23,9 +27,5 @@ class Program {
             break;
          }
       }
-   }
-
-   public static void Main () {
-      Run ();
    }
 }
