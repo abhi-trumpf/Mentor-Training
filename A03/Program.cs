d7f4e3d3450767939c9c23087a0a74dfb368447a
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-09-07 15:23:19 +0530

    A03:added methods and optimized code

--- A03/Program.cs
+++ A03/Program.cs
@@ -3,43 +3,56 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------------------------------------
 // Program.cs
-// Program goes through the words in the .txt file and checks if the word contains the letter 'U'
-// Program also checks if the word is at least 4 letters long and has some combination of the letters 'U', 'X', 'A', 'L', 'T', 'N', and 'E'.
-// It also checks if the word entered by the user is valid or not.
-// The program also calculates the score of the word based on its length and whether it is a pangram or not.
-// The program checks if the word is valid and calculates its score accordingly.
+// Program goes through the words in the .txt file and checks if the word contains the required letters
+// Programs checks if it is valid and highlights the word in green if it is a pangram and prints the score of the word
 // ------------------------------------------------------------------------------------------------
 
-
-using static System.Console;
-var path = Path.Combine (AppContext.BaseDirectory, "words.txt");
-var grandTotal = 0;
-var wordScores = new Dictionary<string, (int Score, bool Ispangram)> ();
-foreach (var word in File.ReadLines (path)) {
-   var isPangram = true;
+public class Project1 {
+   string[] lines = File.ReadAllLines ("words.txt");
    char[] letters = ['U', 'X', 'A', 'L', 'T', 'N', 'E'];
-   if (word.Contains ('U') && word.Length >= 4 && word.All (c => "UXALTNE".Contains (c))) {
-      foreach (var letter in letters) {
-         if (!word.Contains (letter)) { //If each specific letter does not get repeated atleast once.
-            isPangram = false;
-            break;
+   Dictionary<string, int> myDictionary = new ();
+   int grandTotal = 0;
+   //for (int i = 0; i < lines.Length; i++)  //lines - total number of lines in the file.
+   public void IsValid () {
+      foreach (var line in lines) {
+         string word = line.ToUpper ();  //converts to cap U
+         if (word.Contains ("U") && word.Length >= 4 && word.All (letters.Contains)) //check for U
+         {
+            int score = line.Length == 4 ? 1 : line.Length; // '=' = if
+            myDictionary[line] = score;
          }
       }
-      if (isPangram)
-         wordScores[word] = (word.Length + 7, true);
-      else
-         wordScores[word] = (word.Length == 4 ? 1 : word.Length, false);
-      int score;
-      if (word.Length == 4) score = 1;
-      else score = word.Length;
-      if (isPangram) score += 7;
-      grandTotal += score;
+   }
+   public void IsPangram () {
+      foreach (var key in myDictionary.Keys.ToList ()) {
+         string word = key.ToUpper ();
+         if (letters.All (c => word.Contains (char.ToUpper (c)))) //only access the ones that have value
+         {
+            myDictionary[key] += 7;
+         }
+      }
+   }
+   public void PrintKeyValuePairs () {
+      foreach (var item in myDictionary.OrderByDescending (x => x.Value)) {
+         string word = item.Key.ToUpper (); //toUpper()
+         bool isPangram = letters.All (word.Contains); //check if pangram
+         if (isPangram) {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine ($"{item.Value}. {item.Key}");
+            Console.ResetColor ();
+            grandTotal += item.Value;
+         } else {
+            Console.WriteLine ($"{item.Value}. {item.Key}");
+            grandTotal += item.Value;
+         }
+      }
+      Console.WriteLine ("- - - -");
+      Console.WriteLine ($"{grandTotal} total");
+   }
+   public static void Main (string[] args) {
+      Project1 p = new Project1 ();
+      p.IsValid ();
+      p.IsPangram ();
+      p.PrintKeyValuePairs ();
    }
 }
-foreach (var item in wordScores.OrderByDescending (item => item.Value)) {
-   if (item.Value.Ispangram) ForegroundColor = ConsoleColor.Green;
-   WriteLine ($"{item.Value.Score}.{item.Key}");
-   ResetColor ();
-}
-WriteLine ($"----");
-WriteLine ($"{grandTotal} total");
