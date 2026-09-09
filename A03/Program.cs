8eae2ac53b2a2c565074eed6ccf920109f06c893
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-09-09 18:48:42 +0530

    used deconstruction logic to simplify code

--- A03/Program.cs
+++ A03/Program.cs
@@ -3,54 +3,42 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------------------------------------
 // Program.cs
-// Program goes through the words in the .txt file and checks if the word contains the required letters
-// Programs checks if it is valid and highlights the word in green if it is a pangram and prints the score of the word
+// Program goes through the words in the .txt file and checks if the word contains the required
+// letters, Programs checks if it is valid and highlights the word in green if it is a pangram and
+// prints the score of the word
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 
-public class Project1 {
-   string[] lines = File.ReadAllLines ("words.txt");
-   char[] letters = ['U', 'X', 'A', 'L', 'T', 'N', 'E'];
-   Dictionary<string, int> myDictionary = new ();
-   int grandTotal = 0;
-   //for (int i = 0; i < lines.Length; i++)  //lines - total number of lines in the file.
-   public void IsValid () {
-      foreach (var line in lines) {
-         string word = line.ToUpper ();  //converts to cap U
-         if (word.Contains ("U") && word.Length >= 4 && word.All (letters.Contains)) {/*check for U*/
-            int score = line.Length == 4 ? 1 : line.Length; // '=' = if
-            myDictionary[line] = score;
-         }
-      }
+string[] words = File.ReadAllLines ("words.txt");
+char[] letters = ['U', 'X', 'A', 'L', 'T', 'N', 'E'];
+Dictionary<string, (int Score, bool IsGuessed)> gameTracker = [];
+const int minLength = 4;
+int total = 0;
+foreach (var word in words)
+   if (IsValid (word))
+      CalculateScore (word);
+DisplayOutput ();
+
+// Display the value and key pair
+void DisplayOutput () {
+   foreach (var (value,word) in gameTracker.OrderByDescending (x => x.Value.Score)) {
+      if (gameTracker[value].IsGuessed)
+         ForegroundColor = ConsoleColor.Green;
+      WriteLine ($"{word.Score,2}. {value}"); //deconstruction
+      ResetColor ();
    }
-   public void IsPangram () {
-      foreach (var key in myDictionary.Keys.ToList ()) {
-         string word = key.ToUpper ();
-         if (letters.All (c => word.Contains (char.ToUpper (c)))) { /*only access the ones that have value*/
-            myDictionary[key] += 7;
-         }
-      }
-   }
-   public void PrintKeyValuePairs () {
-      foreach (var item in myDictionary.OrderByDescending (x => x.Value)) {
-         string word = item.Key.ToUpper (); //toUpper()
-         bool isPangram = letters.All (word.Contains); //check if pangram
-         if (isPangram) {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine ($"{item.Value}. {item.Key}");
-            Console.ResetColor ();
-            grandTotal += item.Value;
-         } else {
-            Console.WriteLine ($"{item.Value}. {item.Key}");
-            grandTotal += item.Value;
-         }
-      }
-      Console.WriteLine ("- - - -");
-      Console.WriteLine ($"{grandTotal} total");
-   }
-   public static void Main (string[] args) {
-      Project1 p = new Project1 ();
-      p.IsValid ();
-      p.IsPangram ();
-      p.PrintKeyValuePairs ();
-   }
+   WriteLine ($"- - - -\n{total} total");
 }
+
+// Calculates the score of the word and adds it to the total score
+void CalculateScore (string word) {
+   bool isGuessed = letters.All (word.Contains);
+   int score = word.Length == minLength ? 1 : word.Length;
+   if (isGuessed)
+      score += 7;
+   gameTracker[word] = (score, isGuessed);
+   total += score;
+}
+
+// Checks if the word is valid based on the length and returns a boolean value
+bool IsValid (string word) => word.Length >= minLength && word.Contains (letters[0]) && word.All (letters.Contains);
